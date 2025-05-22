71e9a83d62b4297610ad3567a01e0329efb3da00
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-23 00:45:50 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,3 @@
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("admin");
-  eleventyConfig.addPassthroughCopy("src/styles");
-  return {
-    dir: {
-      input: "src",
-      includes: "_includes",
-      data: "../content",
-      output: "_site"
-    }
-  };
 };
