a8ffab08d93f7a407f6bdd4d7f4cdef67493e407
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-23 00:49:34 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,13 @@
 module.exports = function(eleventyConfig) {
+  // Netlify CMS ke liye admin folder ko output (_site/admin) me copy karega
   eleventyConfig.addPassthroughCopy("admin");
+
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
+    }
+  };
 };
