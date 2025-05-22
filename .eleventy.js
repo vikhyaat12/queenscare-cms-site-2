cf4b3abfcf4b7d3f8ee480711948bd30d43a0c77
vikhyaat12 <vikhyaat.sikand@gmail.com>
2025-05-23 01:43:05 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
   // Netlify CMS ke liye admin folder ko output (_site/admin) me copy karega
   eleventyConfig.addPassthroughCopy("admin");
+
+  // Date filter for Nunjucks templates
+  eleventyConfig.addFilter("date", (dateObj, format = "yyyy-LL-dd") => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat(format);
+  });
 
   return {
     dir: {
